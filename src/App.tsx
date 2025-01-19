11bc874690a9bcc0cfb157f5367e49d287fed789
Garu2807 <arutyunyangagik@gmail.com>
2025-01-20 02:41:52 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Global, Main } from './App.styles';
 import CatList from './features/cats/CatList';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
 import NavBar from './features/navbar/NavBar';
 import FavouritesCats from './features/favourites/FavouritesCats';
 
@@ -8,7 +8,7 @@ function App() {
   console.log('App component rendered');
 
   return (
-    <Router basename="/frontend-cats-challenge/">
+    <HashRouter basename="/frontend-cats-challenge/">
       <Global />
       <Main>
         <NavBar />
@@ -17,7 +17,7 @@ function App() {
           <Route path="/likes" element={<FavouritesCats />} />
         </Routes>
       </Main>
-    </Router>
+    </HashRouter>
   );
 }
 //
